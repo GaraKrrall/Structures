9a82c72fd4a6310622825625202b4dbffbc57379
GaraKrrall <akglsms@gmail.com>
2025-12-09 22:52:23 +0300

    1.4

--- common/src/main/java/mc/garakrral/structures/CommonClass.java
+++ common/src/main/java/mc/garakrral/structures/CommonClass.java
@@ -1,13 +1,9 @@
 package mc.garakrral.structures;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class CommonClass {
 
     public static final String MOD_ID = "structures";
     public static final String MOD_NAME = "Structures";
-    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
     public static void init() {
 
